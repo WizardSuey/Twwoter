ca1be2da7842cd1349fc7b7c4f5b61cd5a911b9a
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-16 13:41:32 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,25 +12,4 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
     validates :avatar, presence: true
     
-    def avatar=(input_data)
-    if input_data.nil?
-      self[:avatar] = nil
-    elsif input_data.respond_to?(:read)
-      self[:avatar] = input_data.read
-      self.avatar_data.content_type = input_data.content_type.chomp
-    else
-      logger.error("Bad image data: #{input_data.class.name}: #{input_data.inspect}")
-      raise "Bad image data"
-    end
-  end
-
-  def avatar
-    if self[:avatar].nil?
-      nil
-    else
-      StringIO.new(Base64.decode64(self[:avatar]))
-    end
-  end
-end
-    
 end
