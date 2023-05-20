4cf6b56d24aa08ea4254bb34500f88eaf1f848ec
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-20 19:39:30 +0700

    Update avatar_uploader.rb

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,7 +10,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/"
+    "/assets/images/"
   end
 
   def extension_white_list
