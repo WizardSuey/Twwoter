648996eb29f31e8c62889cfb5fe1ba74eb2784d6
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-20 20:47:58 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,20 @@
 # encoding: utf-8
 
 class User < ApplicationRecord
-    mount_uploader :avatar, AvatarUploader
+    #mount_uploader :avatar, AvatarUploader
     has_many :tweets
     has_one_attached :avatar
     has_many :likes
     has_many :posts
     has_secure_password
     
-#     def avatar=(input_data)
-#         self.avatar_data = input_data.read
-#       end
+    def avatar=(input_data)
+        self.avatar_data = input_data.read
+      end
     
-#       def avatar
-#         StringIO.new(avatar_data)
-#     end
+      def avatar
+        StringIO.new(avatar_data)
+    end
 
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
     validates :avatar, presence: true
