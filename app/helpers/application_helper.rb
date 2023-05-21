1242add26175650942dd1d8ee3b7f32faf40ded5
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-22 01:15:31 +0700

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+   def abbreviated_number(number)
+        if number >= 1_000_000
+          "#{(number / 1_000_000.0).round(1)}млн"
+        elsif number >= 1_000
+          "#{(number / 1_000.0).round(1)}к"
+        else
+          number.to_s
+        end
+    end
 end
