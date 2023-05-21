f234ef4bdb260cfdaaad5ea46d1611dfd9720b0a
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-22 01:31:00 +0700

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,18 @@ class PostsController < ApplicationController
     end
   end
   
-  
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
 
   def like
     @post = Post.find(params[:id])
