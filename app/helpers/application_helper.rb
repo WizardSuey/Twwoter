f9f5565bbaeb8d8d6d047aa4e5a303aa16dbb5d1
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-22 01:29:25 +0700

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
           number.to_s
         end
     end
+   def abbreviated_likes_count(post)
+        likes_count = post.likes_count
+        if likes_count >= 1_000_000
+          "#{(likes_count / 1_000_000.0).round(1)}млн"
+        elsif likes_count >= 1_000
+          "#{(likes_count / 1_000.0).round(1)}к"
+        else
+          likes_count.to_s
+        end
+    end
 end
