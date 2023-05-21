56d4751bfedc7ae82ba2237e4411d539c2d6a92e
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-21 18:48:14 +0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,18 @@ class UsersController < ApplicationController
     @posts = Post.all
     @user = User.find(params[:id])
   end
+  
+  def subscribe
+    @user = User.find(params[:id])
+    Current.user.subscribers.create(user_id: Current.user.id, user_id2: @user.id)
+    @user.increment!(:subscribers_count)
+    redirect_to request.referrer
+  end
+
+  def unsubscribe
+    @user = User.find(params[:id])
+    Current.user.subscribers.find_by(user_id2: @user.id).destroy
+    @user.decrement!(:subscribers_count)
+    redirect_to request.referrer
+  end
 end
