5b02c95d9f606ffa54d7db01f6c6403cf1ca9833
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-21 18:16:11 +0700

    Update passwords_controller.rb

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -7,7 +7,7 @@ class PasswordsController < ApplicationController
 
     def update
         if Current.user.update(password_params)
-            redirect_to root_path, notice: 'Пароль успешно изменён.'
+            redirect_to root_path, notice: 'Данные успешно изменены.'
         else
             render :edit
         end
