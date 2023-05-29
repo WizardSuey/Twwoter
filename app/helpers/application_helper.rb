9798fe561d09d627de6158ddae67b5277ef29dd1
ILusha228 <114432256+WizardSuey@users.noreply.github.com>
2023-05-29 21:36:01 +0700

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,23 @@
 module ApplicationHelper
-   def abbreviated_number(number)
-        if number >= 1_000_000
-          "#{(number / 1_000_000.0).round(1)}млн"
-        elsif number >= 1_000
-          "#{(number / 1_000.0).round(1)}к"
-        else
-          number.to_s
-        end
+  def abbreviated_number(number)
+    return number.to_s if number.nil?
+    if number >= 1_000_000
+      "#{(number / 1_000_000.0).round(1)}млн"
+    elsif number >= 1_000
+      "#{(number / 1_000.0).round(1)}к"
+    else
+      number.to_s
     end
-   def abbreviated_likes_count(post)
-        likes_count = post.likes_count
-        if likes_count >= 1_000_000
-          "#{(likes_count / 1_000_000.0).round(1)}млн"
-        elsif likes_count >= 1_000
-          "#{(likes_count / 1_000.0).round(1)}к"
-        else
-          likes_count.to_s
-        end
+  end
+
+  def abbreviated_likes_count(post)
+    likes_count = post.likes_count || 0
+    if likes_count >= 1_000_000
+      "#{(likes_count / 1_000_000.0).round(1)}млн"
+    elsif likes_count >= 1_000
+      "#{(likes_count / 1_000.0).round(1)}к"
+    else
+      likes_count.to_s
     end
+  end
 end
